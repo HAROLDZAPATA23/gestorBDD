21f15a90756447f6f143502909ede57ce6b9e3f2
Hollman Jhosuhar Camacho Grajales <hjcamacho338@misena.edu.co>
2023-03-29 10:29:15 -0500

    formulario

--- gestor/urls.py
+++ gestor/urls.py
@@ -5,6 +5,8 @@ from. import views
 urlpatterns = [
     path('',views.inicio, name='inicio'),
     path('listado_productos',views.list_productos, name='Calculo'),
+    path('formulario/',views.formulario, name='formulario'),
+    path('respuesta/',views.respuesta, name='respuesta'),
 
 
 ]
--- gestor/views.py
+++ gestor/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
 def inicio(request):
     contexto={}
@@ -9,3 +9,15 @@ def list_productos(request):
     productos=['Portatil lenovo','Iphone galaxiA35','Rolex titan','Motog7']
     contexto={ 'listado':productos}
     return render(request,'list_productos.html', contexto)
+
+def formulario(request):
+    return render(request,'formulario.html')
+
+def respuesta(request):
+
+    nombre=request.GET["nombre"]
+    nacimiento=request.GET["nacimiento"]
+    genero=request.GET["genero"]
+    mensaje="Hola %r Biembenido, tu fecha de nacimiento es %r ,  y tu genero es %r " %(nombre, nacimiento, genero)
+
+    return HttpResponse(mensaje)
