16c36a257f96c2e9f22ad61a6e128bb2d3404bb0
Hollman Jhosuhar Camacho Grajales <hjcamacho338@misena.edu.co>
2023-03-29 11:34:55 -0500

    hello

--- gestor/views.py
+++ gestor/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 # Create your views here.
 def inicio(request):
     contexto={}
@@ -14,10 +15,23 @@ def formulario(request):
     return render(request,'formulario.html')
 
 def respuesta(request):
+    agnoActual=datetime.now().year
+    diaActual=datetime.now().day
+    mesActual=datetime.now().month
+    agnoNacimiento= int(request.GET["nacimiento"].split('-')[0])
+    mesNacimiento=int(request.GET["nacimiento"].split('-')[1])
+    diaNacimiento=int(request.GET["nacimiento"].split('-')[2])
+
+    edad=agnoActual - agnoNacimiento
+    if mesNacimiento > mesActual:
+        edad-=1
+    elif mesActual==mesNacimiento:
+        if diaActual < diaNacimiento:
+            edad-=1
+
 
     nombre=request.GET["nombre"]
-    nacimiento=request.GET["nacimiento"]
     genero=request.GET["genero"]
-    mensaje="Hola %r Biembenido, tu fecha de nacimiento es %r ,  y tu genero es %r " %(nombre, nacimiento, genero)
+    return render(request,'respu.html',{'nombre':nombre,'edad':edad, 'genero':genero})
 
-    return HttpResponse(mensaje)
+
